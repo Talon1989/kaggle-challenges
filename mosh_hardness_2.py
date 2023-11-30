ecf566783dd1dc24035e603eceba6d409e8e5414
fabio <olivetto1989@mail.com>
2023-11-30 16:27:45 +0100

    .

--- mosh_hardness_2.py
+++ mosh_hardness_2.py
@@ -8,43 +8,67 @@ import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
 from tqdm import tqdm
 from sklearn.metrics import r2_score
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 '''
 https://www.kaggle.com/competitions/playground-series-s3e25/overview
 use regression to predict the Mohs hardness of a mineral, given its properties.
-try using a classifier from 0 to 10 with 0.5 increments
 '''
 
 
-hardness = pd.read_csv('data/mohs-hardness/train.csv')
-hardness_test = pd.read_csv('data/mohs-hardness/test.csv')
+train_data = pd.read_csv('data/mohs-hardness/train.csv')
+test_data = pd.read_csv('data/mohs-hardness/test.csv')
 
 
 # print(hardness.isna().sum())
 # print(hardness_test.isna().sum())
 
 
-correlation_matrix = hardness.corr()
+correlation_matrix = train_data.corr()
+skewness = train_data.skew()
 
 
-# encoder = LabelEncoder()
-# encoder.fit(np.arange(0, 10, 0.5))
-def round_to_nearest_half(value):
-    return round(value * 2) / 2
+train_data['Dataset'] = 'Train'
+test_data['Dataset'] = 'Test'
+variables = [feature for feature in train_data.columns if feature not in ['id', 'Hardness', 'Dataset']]
 
 
-hardness['Hardness'].apply(round_to_nearest_half)
+def create_variable_plots(variable):
+    custom_palette = ['#3498db', '#e74c3c']
+    sns.set_style('whitegrid')
+    fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+
+    # Box plot
+    plt.subplot(1, 2, 1)
+    sns.boxplot(data=pd.concat([train_data, test_data], axis=0), x=variable, y="Dataset")
+    plt.xlabel(variable)
+    plt.title(f"Box Plot for {variable}")
+
+    # Separate Histograms
+    plt.subplot(1, 2, 2)
+    sns.histplot(data=train_data, x=variable, color=custom_palette[0], kde=True, bins=30, label="Train")
+    sns.histplot(data=test_data, x=variable, color=custom_palette[1], kde=True, bins=30, label="Test")
+    plt.xlabel(variable)
+    plt.ylabel("Frequency")
+    plt.title(f"Histogram for {variable} [TRAIN & TEST]")
+    plt.legend()
+    # Adjust spacing between subplots
+    plt.tight_layout()
+    # Show the plots
+    plt.show()
 
 
-X_ = hardness.drop('Hardness', axis=1).to_numpy()
-y_ = hardness['Hardness'].to_numpy().reshape([-1, 1])
-X_ = X_.astype(float)
-y_ = y_.astype(float)
+# for v in variables:
+#     create_variable_plots(v)
 
 
-X_train, X_test, y_train, y_test = train_test_split(X_, y_, train_size=75/100)
-train_dataset = TensorDataset(
-    torch.tensor(X_train, dtype=torch.float64), torch.tensor(y_train, dtype=torch.float64))
-train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-in_dim = X_.shape[-1]
+# create_variable_plots('Hardness')
+
+
+# 5 highest hardness correlated features
+important_features = correlation_matrix['Hardness'].abs().sort_values(ascending=False).index[1:6]
+important_features = list(important_features)
